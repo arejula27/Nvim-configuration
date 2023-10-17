bc591887a3deaffa5471f09da6a3a184d0faa361
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-10-17 22:12:41 +0200

    launch telescope if open a dir

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("arejula27.set")
 require("arejula27.remap")
+require("arejula27.bootstrap")
 
---
+++ lua/arejula27/bootstrap.lua
@@ -0,0 +1,22 @@
+-- bootstrap file this while  initiate vim
+local is_git_dir = function()
+  return os.execute('git rev-parse --is-inside-work-tree >> /dev/null 2>&1')
+end
+
+--This function is binded ti VimEnter, which is call when vim loads.
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    local bufferPath = vim.fn.expand('%:p')
+    if vim.fn.isdirectory(bufferPath) ~= 0 then
+      local ts_builtin = require('telescope.builtin')
+      vim.api.nvim_buf_delete(0, { force = true })
+      if is_git_dir() == 0 then
+        ts_builtin.git_files({ show_untracked = true })
+      else
+        ts_builtin.find_files()
+      end
+    end
+  end,
+})
+
+
