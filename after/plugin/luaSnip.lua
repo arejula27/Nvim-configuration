634f8d0e7e29721889b636bc6bc390a5b7c9b64f
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-10-18 23:44:30 +0200

    snipets keymaps

--- after/plugin/luaSnip.lua
+++ after/plugin/luaSnip.lua
@@ -1,5 +1,12 @@
 
+local nnoremap = require("arejula27.keymap").nnoremap
+local inoremap = require("arejula27.keymap").inoremap
 
 --require("luasnip.loaders.from_vscode").load({ include = { "python","golang","lua" } }) -- Load only python snippets
 require("luasnip.loaders.from_vscode").lazy_load({})
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+inoremap("<c-j>","<cmd>lua require'luasnip'.jump(1)<CR>",opts)
+inoremap("<c-k>","<cmd>lua require'luasnip'.jump(-1)<CR>",opts)
 
