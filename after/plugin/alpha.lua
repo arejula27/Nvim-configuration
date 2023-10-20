918c10efccc09e9f01f950ecfc22e9a077ef2415
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-10-20 18:23:20 +0200

    dashboard configurado

--- after/plugin/alpha.lua
+++ after/plugin/alpha.lua
@@ -3,10 +3,23 @@ local alpha = require('alpha')
 
 local dashboard = require'alpha.themes.dashboard'
 -- This is the header, do something fancy
-dashboard.section.header.val = 'foo'
+local dashboard_custom_header = {
+  ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+  ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+  ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+  ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+  ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+  ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+}
+
+dashboard.section.header.val = dashboard_custom_header
 dashboard.section.buttons.val = {
-        dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
-        dashboard.button( "q", "  Quit NVIM" , ":qa<CR>"),
+        dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
+        dashboard.button( "f", "󰮗  Find files" , ":Telescope find_files <CR>"),
+        dashboard.button( "r", "  Recent files" , ":Telescope oldfiles <CR>"),
+        --dashboard.button( "p", "   Projects" , ":Telescope projects <CR>"),
+        dashboard.button( "c", " Configuration" , ":cd ~/.config/nvim | :Telescope find_files <CR>"),
+        dashboard.button( "q", "󰩈  Quit NVIM" , ":qa<CR>"),
         }
 dashboard.section.footer.val = 'Íñigo Aréjula ----- @arejula27'
 alpha.setup(require'alpha.themes.dashboard'.config)
